4207c7ce50d69c939a6dc8f582df7bbcff295dfe
Masaaki Hijikata <hijimasa@gmail.com>
2024-03-13 20:18:24 +0900

    remove material because add more rigid body

--- isaac_ros2_scripts/isaac_scripts/spawn.py
+++ isaac_ros2_scripts/isaac_scripts/spawn.py
@@ -40,16 +40,16 @@ def main(urdf_path:str, x=0.0, y=0.0, z=0.0, roll=0.0, pitch=0.0, yaw=0.0, fixed
         break
 
     # create a rigid body physical material
-    material = PhysicsMaterial(
-        prim_path="/World/physics_material/urdf_default_material",  # path to the material prim to create
-        dynamic_friction=0.4,
-        static_friction=1.1,
-        restitution=0.0
-    )
-    prim_path_list = search_joint_and_link.get_link_prim_path_list(kinematics_chain, "/World/" + robot_name + "/")
-    for prim_path in prim_path_list:
-        prim = GeometryPrim(prim_path, collision=True)
-        prim.apply_physics_material(material)
+    #material = PhysicsMaterial(
+    #    prim_path="/World/physics_material/urdf_default_material",  # path to the material prim to create
+    #    dynamic_friction=0.4,
+    #    static_friction=1.1,
+    #    restitution=0.0
+    #)
+    #prim_path_list = search_joint_and_link.get_link_prim_path_list(kinematics_chain, "/World/" + robot_name + "/")
+    #for prim_path in prim_path_list:
+    #    prim = GeometryPrim(prim_path, collision=True)
+    #    prim.apply_physics_material(material)
     
     stage = omni.usd.get_context().get_stage()
 
